fdff5ffef8bfa71b90da17e9496490078bc48f5f
Dinah-Zoe Neumaier <dine6743@bht-berlin.de>
2025-10-08 16:22:29 +0200

    aufgabe 1 erledigt

--- Uebung11/app/aufgabe1_textformatierung/src/main/java/com/example/aufgabe1_textformatierung/MainActivity.java
+++ Uebung11/app/aufgabe1_textformatierung/src/main/java/com/example/aufgabe1_textformatierung/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.aufgabe1_textformatierung;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.widget.TextView;
 import android.graphics.Typeface;
 
@@ -14,9 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Beispiel: Eigene Schriftart verwenden
-        TextView customFontText = findViewById(R.id.customFontText);
-        Typeface schriftart = Typeface.createFromAsset(getAssets(), "fonts/deineschrift.ttf");
-        customFontText.setTypeface(schriftart);
+
     }
 }
